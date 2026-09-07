983cb5c3d0579726b7ecc68a93952de71d4556ad
CommanderMC <cyanminecraft2023@outlook.com>
2026-09-07 08:47:35 +0100

    Fix?

--- windows/BUILD_ATHENA_EXE.bat
+++ windows/BUILD_ATHENA_EXE.bat
@@ -3,7 +3,7 @@ setlocal
 cd /d "%~dp0"
 where py >nul 2>&1 || (echo Python is required for the BUILD step only. & pause & exit /b 1)
 py -m pip install --upgrade pyinstaller
-py -m PyInstaller --noconfirm --clean --onefile --windowed --name Athena athena_pc.py
+py -m PyInstaller --noconfirm --clean --onefile --windowed --name Athena --icon athena.ico athena_pc.py
 if exist "dist\Athena.exe" (
   echo.
   echo SUCCESS: dist\Athena.exe
